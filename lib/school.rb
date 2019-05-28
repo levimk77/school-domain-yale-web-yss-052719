d144dd483f1fb40de9650a64539201460801aa7e
Elijah Florence <efloren@siue.edu>
2019-05-28 18:43:51 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
-# code here!
+class School
+  attr_accessor :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(studentName,grade)
+   if @roster[grade] == nil
+    @roster[grade] = []
+    @roster[grade] << studentName
+  else
+     @roster[grade] << studentName
+   end
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+ 
+  def sort 
+    sortedArray = {}
+    @roster.map do |key, value|
+      sortedArray[key] = 
+      sortedArray[key] = @roster[key].sort
+    end
+    return sortedArray
+  end
+  
+  
+  
+
+
+end
