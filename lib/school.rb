c483caa8633898da7ccf3fb09f29b061dfb86e9e
Elijah Florence <efloren@siue.edu>
2019-05-28 18:28:56 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+  attr_accessor :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(studentName,grade)
+   if @roster[grade] == nil
+    @roster[grade] = []
+    @roster[grade] << studentName
+  else
+     @roster[grade] << studentName
+   end
+  end
+ 
+
+  
+  
+  
+
+
+end
